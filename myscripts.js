761b7e2f39110900314424c1cf56b07dc53c088a
Nassanalytics <nassim.naoui@gmail.com>
2023-11-03 22:29:44 +0100

    Ajout fonctionnalité des createGrid, clear et clearAll

--- myscripts.js
+++ myscripts.js
@@ -1,5 +1,5 @@
 function grid () {
-    number = prompt()
+    number = prompt("Choose the number of squares")
     if (number > 100 || number < 1) {
         alert("The number should be between 1 and 100");
     } else {
@@ -19,18 +19,30 @@ function createGrid () {
     for (let i = 0; i < number; i++) {
         const div = document.createElement('div');
         div.classList.add('square');
+        var dimension = (100/Math.sqrt(number))
+        div.style.height=  `${dimension}%`;
+        div.style.width=  `${dimension}%`;
         container.appendChild(div);
-    };
+    };  const square = document.querySelectorAll('.square');
+        square.forEach(square => {
+        square.addEventListener('mouseover',etching)
+});     square.forEach(square => {
+        square.addEventListener('click',clear)
+});     const clearButton = document.getElementById('right-button');
+        clearButton.addEventListener("click", function() {
+        square.forEach(square => {
+        clearAll(square)
+    });
+})
 }
 
-for (let i = 0; i < (16**2); i++) {
+/*for (let i = 0; i < (16**2); i++) {
     const div = document.createElement('div');
     div.classList.add('square');
     container.appendChild(div);
-};
+};*/
 
 const square = document.querySelectorAll('.square');
-
 square.forEach(square => {
     square.addEventListener('mouseover',etching)
 });
@@ -39,3 +51,24 @@ function etching() {
     this.style.backgroundColor = '#141E46';
 }
 
+const clearButton = document.getElementById('right-button');
+clearButton.addEventListener("click", function() {
+    square.forEach(square => {
+        clearAll(square)
+    });
+})
+
+square.forEach(square => {
+    square.addEventListener('click',clear)
+});
+
+function clearAll (div) {
+        div.style.backgroundColor = '#efe5e5';
+}
+
+function clear (div) {
+    this.style.backgroundColor = '#efe5e5';
+}
+
+
+/*#efe5e5*/
